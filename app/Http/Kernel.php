0332222a12b9dd95dc9f0a03c6868706dcb6feb1
pisey <pisey@marinax.biz>
2018-04-26 10:53:06 +0700

    OAuth2 - Creating and setting tokens

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,8 @@ class Kernel extends HttpKernel
             'bindings',
             \App\Http\Middleware\Loggin::class,
             'httpHeaders:Come work for us!',
-            \App\Http\Middleware\TokenAuth::class,
+            // \App\Http\Middleware\TokenAuth::class,
+            'auth:api'
             
             
         ],
